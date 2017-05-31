5ee820f814530cef9c134f038405f40e75dad5b8
Todd A. Anderson <drtodd13@comcast.net>
2017-05-30 19:42:36 -0700

    Check for WF and LF.

--- allstars/extract_cumul_tourney_reports.sh
+++ allstars/extract_cumul_tourney_reports.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-awk '/pos last name first/{flag=1;next}/The total Number/{flag=0}flag' $1 | cut -c28- | sed 's/\// /' | awk '{ num_games = 0; for(i=5; i < NF; i++) { if ($i ~ /[WLD]/) num_games=num_games+1;} print $1 " " $2 " " $3 " " $4 " " num_games}'
+awk '/pos last name first/{flag=1;next}/The total Number/{flag=0}flag' $1 | cut -c28- | sed 's/\// /' | awk '{ num_games = 0; for(i=5; i < NF; i++) { if (($i ~ /[WLD]/) && ($i != "WF") && ($i != "LF")) num_games=num_games+1;} print $1 " " $2 " " $3 " " $4 " " num_games}'
 #awk '/pos last name first/{flag=1;next}/The total Number/{flag=0}flag' $1 | cut -c28- | sed 's/\// /' | cut -c1-24 | awk '{if ($4 >= 15) print $0}'

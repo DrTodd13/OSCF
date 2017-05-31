8b9331dc3d504c4adc80707c4e4f865d886f8bef
Todd A. Anderson <drtodd13@comcast.net>
2017-05-30 19:42:02 -0700

    Improvements.

--- allstars/allstars.cc
+++ allstars/allstars.cc
@@ -1,8 +1,6 @@
-#include "DatFile.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <chrono>
 #include <unistd.h>
 #include <algorithm>
 #include <map>
@@ -10,21 +8,9 @@
 #include <sstream>
 #include <set>
 #include <sstream>
-
-int daysdiff(const std::string &in, const std::chrono::system_clock::time_point &now) {
-    std::tm tm = {0,0,0,0,0,0,0,0,0,0,0};
-    tm.tm_mon = atoi(in.substr(0,2).c_str()) - 1;
-    tm.tm_mday = atoi(in.substr(3,2).c_str());
-    tm.tm_year = 100 + atoi(in.substr(6,2).c_str());
-//    std::cout << tm.tm_mon << " " << tm.tm_mday << " " << tm.tm_year << std::endl;
-
-    std::time_t tt=mktime(&tm);
-    std::chrono::system_clock::time_point then = std::chrono::system_clock::from_time_t(tt);
-    int ret = std::chrono::duration_cast<std::chrono::hours>(now - then).count();
-    ret /= 24; // convert to days
-//    std::cout << "date = " << in << " day diff = " << ret << std::endl;
-    return ret;
-}
+#include "DatFile.cpp"
+#include <string.h>
+#include <assert.h>
 
 class indirect_sort {
 public:
@@ -36,18 +22,62 @@ public:
 
 #define ESTABLISHED_RATING 15
 
-void process_tourney_results(const std::string &file, std::vector<Player> &or_players, std::map<std::string, unsigned> &nwsrs_map) {
+class MyDate {
+public:
+    int month, day, year;
+    bool good;
+
+    MyDate(const char *in) {
+	good = true;
+        int len = strlen(in);
+	if (len != 8) {
+//            if (debug) {
+//		    std::cout << "Bad date string " << in << std::endl;
+//	    }
+            good = false;
+	}
+	month = atoi(in);
+	day = atoi(in+3);
+	year = atoi(in+6);
+	if (year < 100) {
+	    year += 2000;
+        }
+    }
+
+    bool operator<(const MyDate &rhs) const {
+        if (year < rhs.year) return true;
+        if (year > rhs.year) return false;
+        if (month < rhs.month) return true;
+        if (month > rhs.month) return false;
+        if (day < rhs.day) return true;
+        if (day > rhs.day) return false;
+	return false;
+    }
+    friend std::ostream & operator<<(std::ostream &os, const MyDate &md);
+};
+
+std::ostream & operator<<(std::ostream &os, const MyDate &md) {
+    os << md.month << "/" << md.day << "/" << md.year;
+    return os;
+}
+
+void process_tourney_results(const std::string &file, std::vector<Player> &or_players, std::map<std::string, unsigned> &nwsrs_map, std::ofstream &logfile) {
     std::ifstream tfile(file);
     while(!tfile.eof()) {
-        std::string id;
+        std::string fullid;
         unsigned start_rating, end_rating, total_games, games_this_event;
-        tfile >> id >> start_rating >> end_rating >> total_games >> games_this_event; 
+        tfile >> fullid >> start_rating >> end_rating >> total_games >> games_this_event; 
         if (tfile.eof()) break;
 
         unsigned games_at_start = total_games - games_this_event;
+
+        logfile << "process " << fullid << " " << start_rating << " " << end_rating << " " << total_games << " " << games_this_event << " " << games_at_start << std::endl;
+	assert(fullid.length() == 8);
+	std::string id = fullid.substr(4,4);
+
         auto player_index = nwsrs_map.find(id);
         if (player_index == nwsrs_map.end()) {
-//            std::cerr << "Player id " << id << " not found in nwsrs_map." << std::endl;
+            logfile << "Player id " << id << " not found in nwsrs_map." << std::endl;
             continue;
         }
         
@@ -66,32 +96,48 @@ void process_tourney_results(const std::string &file, std::vector<Player> &or_pl
     }
 }
 
-void process_uscf_supp(const std::string &file, std::vector<Player> &or_players, std::map<std::string, unsigned> &uscf_map, std::set<std::string> &uscf_but_not_nwsrs) {
+void process_uscf_supp(const std::string &file, std::vector<Player> &or_players, std::map<std::string, unsigned> &uscf_map, std::set<std::string> &uscf_but_not_nwsrs, const MyDate &start_date, std::ofstream &logfile) {
     std::ifstream tfile(file);
     while(!tfile.eof()) {
         std::string id;
-        unsigned start_rating;
-        tfile >> id >> start_rating;
+        unsigned rating;
+        tfile >> id >> rating;
         if (tfile.eof()) break;
 
         auto player_index = uscf_map.find(id);
+
+	logfile << "uscf supp " << id << " " << rating << std::endl;
+
         if (player_index == uscf_map.end()) {
-//            std::cerr << "Player id " << id << " not found in uscf_map." << std::endl;
+            logfile << "Player id " << id << " not found in uscf_map." << std::endl;
             continue;
         }
         
         Player &this_player = or_players[player_index->second];
 
-        if (this_player.isAdult()) continue;
-        if (this_player.isUSCFProvOrExpired()) continue;
+	logfile << "Player " << this_player.last_name << " " << this_player.first_name << " " << this_player.calc_highest_rating << " " << this_player.isAdult() << " " << this_player.isUSCFProv() << " " << this_player.uscf_exp_date << std::endl;
 
-        if (start_rating > this_player.calc_highest_rating) {
+        if (this_player.isAdult()) continue;
+        if (this_player.isUSCFProv()) continue;
+        //if (this_player.isUSCFProvOrExpired()) continue;
+	MyDate temp_date(this_player.uscf_exp_date.c_str());
+	logfile << "exp date " << temp_date << " start date " << start_date << " " << (temp_date < start_date) << std::endl;
+	if (temp_date < start_date) continue;
+
+        if (rating > this_player.calc_highest_rating) {
+	    logfile << "USCF supp rating higher than calc_highest_rating." << std::endl;
             // For now just remember if a player has USCF but no NWSRS games.
             if (this_player.calc_highest_rating == 0) {
-                uscf_but_not_nwsrs.insert(this_player.getFullId());                
+                uscf_but_not_nwsrs.insert(this_player.id);                
             }
-            this_player.calc_highest_rating = start_rating;
-        } 
+            this_player.calc_highest_rating = rating;
+	    if (this_player.nwsrs_rating > this_player.calc_highest_rating) {
+	        logfile << "nwsrs rating higher than calc." << std::endl;
+                this_player.calc_highest_rating = this_player.nwsrs_rating;
+	    }
+        } else {
+	    logfile << "USCF supp rating higher than calc_highest_rating." << std::endl;
+	}
     }
 }
 
@@ -131,13 +177,15 @@ std::map<std::string, std::string> load_schoolcodes(void) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
+    if (argc != 6 && argc != 7) {
         std::cerr << "Wrong number of command line arguments." << std::endl;
-        std::cerr << "Correct is: " << argv[0] << " <rateXX-XXX.dat> <cumulative_tourney_results_file> <uscf_supplement>" << std::endl;
+        std::cerr << "Correct is: " << argv[0] << " <rateXX-XXX.dat> <cumulative_tourney_results_file> <uscf_supplement> <start date MM/DD/YYYY> <end date MM/DD/YYYY>" << std::endl;
         exit(-1);
     }
+    
+    MyDate start_date(argv[4]);
+    MyDate end_date(argv[5]);
 
-    //std::ifstream infile("rate16-387.dat");
     std::ifstream infile(argv[1]);
     Player p; 
     int i;
@@ -145,37 +193,44 @@ int main(int argc, char *argv[]) {
     std::vector<Player> or_players;
     std::map<std::string, unsigned> nwsrs_map;
     std::map<std::string, unsigned> uscf_map;
+    std::ofstream logfile("allstars.log");
 
-    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
     unsigned player_index = 0;
+
+    logfile << "All Stars for date range " << start_date << " to " << end_date << std::endl;
+    logfile << "Loading ratings data." << std::endl;
 
     while(!infile.eof()) {
         infile >> p;
         if (!infile.eof()) {
             if (p.state == "OR") {
                 or_players.push_back(p);
-                nwsrs_map.insert(std::pair<std::string,unsigned>(p.getFullId(), player_index));
+                nwsrs_map.insert(std::pair<std::string,unsigned>(p.id, player_index));
                 if (!p.uscf_id.empty()) {
                     uscf_map.insert(std::pair<std::string,unsigned>(p.uscf_id, player_index));
                 }
+		logfile << p.last_name << " " << p.first_name << " " << p.id << " " << p.uscf_id << " " << player_index << std::endl;
                 ++player_index;
             }
         } 
     }
 
-    process_tourney_results(argv[2], or_players, nwsrs_map);
+    logfile << "Done loading ratings data." << std::endl;
+
+    process_tourney_results(argv[2], or_players, nwsrs_map, logfile);
     std::set<std::string> uscf_but_no_nwsrs;
-    process_uscf_supp(argv[3], or_players, uscf_map, uscf_but_no_nwsrs);
+    process_uscf_supp(argv[3], or_players, uscf_map, uscf_but_no_nwsrs, start_date, logfile);
 
     for (i = 0; i < or_players.size(); ++i) {
         int grade_index = or_players[i].grade - 'A';
         if (grade_index < 0 || grade_index > 13) {
-           std::cerr << "Bad grade index." << std::endl;
+           std::cerr << "Bad grade index for player " << or_players[i].last_name << " " << or_players[i].first_name << " grade code = " << or_players[i].grade << std::endl;
            exit(-1);
         }
 
         if (or_players[i].calc_highest_rating > 0) {
             or_by_grade[grade_index].push_back(i);
+            logfile << "Adding " << or_players[i].last_name << " " << or_players[i].first_name << " " << or_players[i].id << " " << or_players[i].uscf_id << " " << i << " to grade index " << grade_index << std::endl;
         }
     }
 
@@ -268,8 +323,8 @@ int main(int argc, char *argv[]) {
             }
 
             std::cout << std::setw(4) << position_str.str() << " " << std::left << std::setw(30) << full_name << " " << std::setw(20) << city << std::right << std::setw(4) << star.calc_highest_rating;
-            if (uscf_but_no_nwsrs.find(star.getFullId()) != uscf_but_no_nwsrs.end()) {
-                std::cout << " Player has USCF supplement but no NWSRS games.";
+            if (uscf_but_no_nwsrs.find(star.id) != uscf_but_no_nwsrs.end()) {
+//                std::cout << " Player has USCF supplement but no NWSRS games.";
             }
             std::cout << std::endl;
         }

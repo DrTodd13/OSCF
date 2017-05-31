48008e13cd292e3f05b54acbcdbff9cd3dbd930c
Todd A. Anderson <drtodd13@comcast.net>
2017-05-30 19:41:01 -0700

    Move implementations to cpp file.

--- allstars/DatFile.h
+++ allstars/DatFile.h
@@ -2,18 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 
-std::string trim(const std::string& str,
-                 const std::string& whitespace = " \t")
-{
-    const size_t strBegin = str.find_first_not_of(whitespace);
-    if (strBegin == std::string::npos)
-        return ""; // no content
-
-    const size_t strEnd = str.find_last_not_of(whitespace);
-    const size_t strRange = strEnd - strBegin + 1;
-
-    return str.substr(strBegin, strRange);
-}
+std::string trim(const std::string& str, const std::string& whitespace = " \t");
 
 class Player {
 public:
@@ -61,82 +50,15 @@ public:
         return uscf_prov_codes == "E" || uscf_prov_codes == "P";
     }
 
+    bool isUSCFProv(void) const {
+        return uscf_prov_codes == "P";
+    }
+
     Player(void) : grade('Z'), nwsrs_rating(0), num_games(0), games_ytd(0), highest_rating_this_year(0), lowest_rating(0), start_rating(0), kirks_code(0), rating_basis(' '), uscf_rating(0), calc_highest_rating(0) {}
 };
 
-std::string read_fixed(std::istream &in, unsigned length) {
-    char buf[128] = "";
-    in.read(buf, length); 
-    return trim(std::string(buf));
-}
-
-char read_fixed_char(std::istream &in, unsigned length) {
-    std::string temp = read_fixed(in, length);
-    return temp[0];
-}
-
-unsigned read_fixed_unsigned(std::istream &in, unsigned length) {
-    std::string temp = read_fixed(in, length);
-    return std::atoi(temp.c_str());
-}
-
-std::istream& operator>>(std::istream &in, Player &p) {
-    p.last_name = read_fixed(in, 25); 
-    p.first_name = read_fixed(in, 25); 
-    p.school_code = read_fixed(in, 4); 
-    p.grade = read_fixed_char(in, 2);
-    p.id = read_fixed(in, 6); 
-    p.nwsrs_rating = read_fixed_unsigned(in, 5); 
-    p.num_games = read_fixed_unsigned(in, 5); 
-    p.games_ytd = read_fixed_unsigned(in, 5); 
-    p.highest_rating_this_year = read_fixed_unsigned(in, 6); 
-    p.lowest_rating = read_fixed_unsigned(in, 6); 
-    p.start_rating = read_fixed_unsigned(in, 6); 
-    p.kirks_code = read_fixed_unsigned(in, 5); 
-    p.last_action_date = read_fixed(in, 9); 
-    p.rating_basis = read_fixed_char(in, 2);
-    p.uscf_last_name = read_fixed(in, 25); 
-    p.uscf_first_name = read_fixed(in, 25); 
-    p.uscf_id = read_fixed(in, 9); 
-    p.uscf_exp_date = read_fixed(in, 9); 
-    p.uscf_rating = read_fixed_unsigned(in, 6); 
-    p.uscf_prov_codes = read_fixed(in, 5); 
-    p.uscf_rating_date = read_fixed(in, 9); 
-    p.state = read_fixed(in, 3);
-    char c;
-    //std::cout << "New line code is " << (int)'\n' << std::endl;
-    do {
-       in.get(c);
-       //std::cout << "Found char " << c << " " << (int)c << std::endl;
-    } while(c != '\n');
-    
-    return in;
-}
-
-std::ostream& operator<<(std::ostream &os, Player &p) {
-    os << p.last_name << " ";
-    os << p.first_name << " ";
-    os << p.school_code << " ";
-    os << p.grade << " ";
-    os << p.id << " ";
-    os << p.nwsrs_rating << " ";
-    os << p.num_games << " ";
-    os << p.games_ytd << " ";
-    os << p.highest_rating_this_year << " ";
-    os << p.lowest_rating << " ";
-    os << p.start_rating << " ";
-    os << p.kirks_code << " ";
-    os << p.last_action_date << " ";
-    os << p.rating_basis << " ";
-    os << p.uscf_last_name << " ";
-    os << p.uscf_first_name << " ";
-    os << p.uscf_id << " ";
-    os << p.uscf_exp_date << " ";
-    os << p.uscf_rating << " ";
-    os << p.uscf_prov_codes << " ";
-    os << p.uscf_rating_date << " ";
-    os << p.state;
-    
-    return os;
-}
-
+std::string read_fixed(std::istream &in, unsigned length);
+char read_fixed_char(std::istream &in, unsigned length);
+unsigned read_fixed_unsigned(std::istream &in, unsigned length);
+std::istream& operator>>(std::istream &in, Player &p);
+std::ostream& operator<<(std::ostream &os, Player &p);
